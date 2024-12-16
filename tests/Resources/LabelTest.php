8f0ab95b91c9a7dbba9deefd92483f0582908ee3
Wim Griffioen <wim@griffioen.me>
2024-12-16 11:26:22 +0100

    Make the parameters nullable
    The parameters are optional in the API, but where required in the SDK

--- tests/Resources/LabelTest.php
+++ tests/Resources/LabelTest.php
@@ -24,7 +24,25 @@ class LabelTest extends TestCase
         $this->assertEquals([], $resource->get(['123456']));
 
         $this->assertEquals(
-            '/api/labels?ids%5B0%5D=123456&paper_type=A6&start_location=top-left',
+            '/api/labels?ids%5B0%5D=123456',
+            (string) $handler->getLastRequest()->getUri()
+        );
+
+        $this->assertEquals('GET', $handler->getLastRequest()->getMethod());
+    }
+
+    public function testParametersAreSetInURL(): void
+    {
+        $handler = new MockHandler([
+            new Response(200, [], json_encode([])),
+        ]);
+
+        $resource = new Label($this->buildConnectionWithMockHandler($handler));
+
+        $this->assertEquals([], $resource->get(['123456', 'A4', 'top-left']));
+
+        $this->assertEquals(
+            '/api/labels?ids%5B0%5D=123456&ids%5B1%5D=A4&ids%5B2%5D=top-left',
             (string) $handler->getLastRequest()->getUri()
         );
 
